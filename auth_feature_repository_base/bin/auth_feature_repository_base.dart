fad227f9367ec0138ed4d042eb5e9965ea1ed3d0
KamiTzayig <Dyekozz@gmail.com>
2023-09-23 10:10:45 +0300

    auth_repository_base first iteration

--- auth_feature_repository_base/bin/auth_feature_repository_base.dart
+++ auth_feature_repository_base/bin/auth_feature_repository_base.dart
@@ -1,5 +1,5 @@
 import 'package:auth_feature_repository_base/auth_feature_repository_base.dart' as auth_feature_repository_base;
 
 void main(List<String> arguments) {
-  print('Hello world: ${auth_feature_repository_base.calculate()}!');
+  print('Hello world: !');
 }
--- auth_feature_repository_base/lib/auth_feature_repository_base.dart
+++ auth_feature_repository_base/lib/auth_feature_repository_base.dart
@@ -1,3 +1,43 @@
-int calculate() {
-  return 6 * 7;
-}
+import 'package:auth_feature_repository_base/user_model.dart';
+
+abstract class AuthFeatureRepositoryBase{
+
+  //example of an abstract repository which the data layer will implement
+  //getters
+  String? get userId;
+
+  bool get isAlreadyLoggedIn;
+
+  String get displayName;
+
+  //Notifies when the authentication status changes.
+  Stream<AuthUser> get authStateChanges;
+
+
+  Future<void> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+  Future<void> verifyEmail({
+    required String email,
+  });
+  Future<void> signInWithGoogle();
+
+  Future<void> signInWithPhoneNumber({
+    required String phoneNumber,
+  });
+
+  Future<void> verifySmsCode({
+    required String smsCode,
+  });
+
+  //Logs out from the service.
+  Future<void> signOut();
+
+  }
+
