4def6b1313839bc1a165bb10d64b19599f37af21
KamiTzayig <Dyekozz@gmail.com>
2023-10-16 00:13:14 +0300

    authfailure code update

--- auth_feature_repository_base/lib/authFailure.dart
+++ auth_feature_repository_base/lib/authFailure.dart
@@ -1,4 +1,4 @@
-class AuthFailure{
+class AuthFailure {
   final String authFailureCode;
   final AuthProviderType authProviderType;
 
@@ -6,8 +6,6 @@ class AuthFailure{
     required this.authFailureCode,
     required this.authProviderType,
   });
-
-
 }
 //replace the following enum with a map to string of the same values
 
@@ -21,37 +19,56 @@ enum AuthProviderType {
   anonymous,
   custom
 }
+
 class AuthFailureCode {
   static const String none = 'none';
   static const String unknown = 'unknown';
-  static const String serverError = 'serverError';
-  static const String tooManyRequests = 'tooManyRequests';
-  static const String invalidMail = 'invalidMail';
-  static const String mailAlreadyInUse = 'mailAlreadyInUse';
-  static const String mailIsNotInUse = 'mailIsNotInUse';
-  static const String invalidPassword = 'invalidPassword';
-  static const String wrongPassword = 'wrongPassword';
-  static const String invalidPhoneNumber = 'invalidPhoneNumber';
-  static const String smsTimeout = 'smsTimeout';
-  static const String sessionExpired = 'sessionExpired';
-  static const String operationNotAllowed = 'operationNotAllowed';
+  static const String server_error = 'serverError';
+  static const String too_many_requests = 'tooManyRequests';
+  static const String invalid_mail = 'invalidMail';
+  static const String mail_already_in_use = 'mailAlreadyInUse';
+  static const String mail_is_not_in_use = 'mailIsNotInUse';
+  static const String invalid_password = 'invalidPassword';
+  static const String wrong_password = 'wrongPassword';
+  static const String invalid_phone_number = 'invalidPhoneNumber';
+  static const String sms_timeout = 'smsTimeout';
+  static const String session_expired = 'sessionExpired';
+  static const String operation_not_allowed = 'operationNotAllowed';
+  static const String weak_password = 'weakPassword';
+  static const String account_exists_with_different_credential =
+      'account-exists-with-different-credential';
+  static const String invalid_credential = 'invalid-credential';
+  static const String user_disabled = 'user-disabled';
+  static const String user_not_found = 'user-not-found';
+  static const String invalid_verification_code = 'invalid-verification-code';
+  static const String invalid_verification_id = 'invalid-verification-id';
 
-  static const String weakPassword = 'weakPassword';
-
-  String fromStringCode(String code){
+  String fromStringCode(String code) {
     switch (code) {
       case 'email-already-in-use':
-        return mailAlreadyInUse;
+        return mail_already_in_use;
       case 'invalid-email':
-        return invalidMail;
+        return invalid_mail;
       case 'operation-not-allowed':
-        return operationNotAllowed;
+        return operation_not_allowed;
       case 'weak-password':
-        return weakPassword;
+        return weak_password;
+      case 'account-exists-with-different-credential':
+        return account_exists_with_different_credential;
+      case 'invalid-credential':
+        return invalid_credential;
+      case 'user-disabled':
+        return user_disabled;
+      case 'user-not-found':
+        return user_not_found;
+      case 'auth/wrong-password':
+        return invalid_password;
+      case 'invalid-verification-code':
+        return invalid_verification_code;
+      case 'invalid-verification-id':
+        return invalid_verification_id;
       default:
         return unknown;
     }
-
   }
 }
-
