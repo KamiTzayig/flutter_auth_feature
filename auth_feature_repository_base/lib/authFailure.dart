a6be7b6baa42f3e639fa4476836fe30c66e5c2b0
KamiTzayig <Dyekozz@gmail.com>
2023-10-15 23:40:25 +0300

    add fromStringCode method to auth failure

--- auth_feature_repository_base/lib/authFailure.dart
+++ auth_feature_repository_base/lib/authFailure.dart
@@ -7,6 +7,7 @@ class AuthFailure{
     required this.authProviderType,
   });
 
+
 }
 //replace the following enum with a map to string of the same values
 
@@ -22,6 +23,7 @@ enum AuthProviderType {
 }
 class AuthFailureCode {
   static const String none = 'none';
+  static const String unknown = 'unknown';
   static const String serverError = 'serverError';
   static const String tooManyRequests = 'tooManyRequests';
   static const String invalidMail = 'invalidMail';
@@ -32,5 +34,24 @@ class AuthFailureCode {
   static const String invalidPhoneNumber = 'invalidPhoneNumber';
   static const String smsTimeout = 'smsTimeout';
   static const String sessionExpired = 'sessionExpired';
+  static const String operationNotAllowed = 'operationNotAllowed';
+
+  static const String weakPassword = 'weakPassword';
+
+  String fromStringCode(String code){
+    switch (code) {
+      case 'email-already-in-use':
+        return mailAlreadyInUse;
+      case 'invalid-email':
+        return invalidMail;
+      case 'operation-not-allowed':
+        return operationNotAllowed;
+      case 'weak-password':
+        return weakPassword;
+      default:
+        return unknown;
+    }
+
+  }
 }
 
