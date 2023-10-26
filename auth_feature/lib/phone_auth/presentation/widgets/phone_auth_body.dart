66fd08c6d1013d015b4fef98c3a815e31f7e0d8e
KamiTzayig <Dyekozz@gmail.com>
2023-10-26 17:21:09 +0300

    phone auth feature auth failure

--- auth_feature/lib/phone_auth/presentation/widgets/phone_auth_body.dart
+++ auth_feature/lib/phone_auth/presentation/widgets/phone_auth_body.dart
@@ -1,5 +1,6 @@
 import 'package:auth_feature/core/models/base_models/phone_auth_state.dart';
 import 'package:auth_feature/phone_auth/presentation/widgets/insert_phone_number.dart';
+import 'package:auth_feature_repository_base/auth_failure.dart';
 import 'package:flutter/material.dart';
 import '../../application/state.dart';
 import 'insert_sms_code.dart';
@@ -11,13 +12,19 @@ class PhoneAuthBody extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final PhoneAuthState state = ref.watch(phoneAuthStateNotifierProvider);
     final bool smsSent = state.smsSent;
+    final bool isLoading = state.isLoading;
+    final AuthFailure authFailure = state.authFailure;
     return Column(
       children: [
         Text("phone auth"),
+        if(isLoading) CircularProgressIndicator(),
         if(!smsSent) InsertPhoneNumber(),
         if(!smsSent) TextButton(onPressed: () => ref.read(phoneAuthStateNotifierProvider.notifier).signInWithPhoneNumber(), child: Text("send sms code")),
         if(smsSent) InsertSmsCode(),
         if(smsSent) TextButton(onPressed: () => ref.read(phoneAuthStateNotifierProvider.notifier).verifySmsCode(), child: Text("sign in with sms code")),
+        if(authFailure.authFailureCode!=AuthFailureCode.none)
+          ...[Text(authFailure.authFailureCode),
+            Text(authFailure.message??'-')]
       ],
     );
   }
