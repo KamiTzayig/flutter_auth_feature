11e1971b3ee59c15e59e1b3402a77efd8245dab6
KamiTzayig <Dyekozz@gmail.com>
2023-11-09 20:29:03 +0200

    update export of auth user

--- auth_feature/lib/auth_feature.dart
+++ auth_feature/lib/auth_feature.dart
@@ -3,3 +3,4 @@ export 'src/google_auth/google_auth.dart';
 export 'src/email_auth/email_auth.dart';
 export 'src/core/auth_feature_class.dart';
 export 'src/core/auth_feature_core.dart';
+export 'package:auth_feature_repository_base/auth_user_model.dart' ;
